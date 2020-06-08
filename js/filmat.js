40e2d0ffc10c73deef1d7073452d6056c77c3478
Esma Krasniqi <44786311+esmakrasniqi@users.noreply.github.com>
2020-06-08 19:00:08 +0200

    Add files via upload

---
+++ js/filmat.js
@@ -0,0 +1,10 @@
+function showDiv(select){
+						   if(select.value==2){
+						    document.getElementById('hidden_div').style.display = "block";
+						   } else if(select.value==1){
+						    document.getElementById('hidden_div1').style.display = "block";
+						   }
+						   else{
+						    document.getElementById('hidden_div').style.display = "none";
+						   }
+						} 
