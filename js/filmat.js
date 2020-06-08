245018d554d1e810cd44bea4081cfd8a3edc3281
Esma Krasniqi <44786311+esmakrasniqi@users.noreply.github.com>
2020-06-08 19:02:53 +0200

    Update filmat.js

--- js/filmat.js
+++ js/filmat.js
@@ -1,8 +1,6 @@
 function showDiv(select){
-						   if(select.value==2){
+						   if(select.value==0){
 						    document.getElementById('hidden_div').style.display = "block";
-						   } else if(select.value==1){
-						    document.getElementById('hidden_div1').style.display = "block";
 						   }
 						   else{
 						    document.getElementById('hidden_div').style.display = "none";
