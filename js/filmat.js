1982de163fe1368fe79bd1f7c8afb4b2bcaaae87
Esma Krasniqi <44786311+esmakrasniqi@users.noreply.github.com>
2020-06-08 22:42:35 +0200

    folder

--- js/filmat.js
+++
@@ -1,8 +0,0 @@
-function showDiv(select){
-						   if(select.value==1){
-						    document.getElementById('hidden_div').style.display = "block";
-						   }
-						   else{
-						    document.getElementById('hidden_div').style.display = "none";
-						   }
-						} 
---
+++ web/js/filmat.js
@@ -0,0 +1,8 @@
+function showDiv(select){
+						   if(select.value==1){
+						    document.getElementById('hidden_div').style.display = "block";
+						   }
+						   else{
+						    document.getElementById('hidden_div').style.display = "none";
+						   }
+						} 
